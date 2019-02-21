e5be26607c49a6c48d50597ff27c427d7fc448df
dctenna <dctenna@gmail.com>
2019-02-21 13:32:33 +0200

    Add eslintrc file, replace var declaration

--- js/script.js
+++ js/script.js
@@ -1,30 +1,46 @@
 let dictionary = {
-	"int" : "var",
-	"float" : "var",
-	"string" : "var",
-	"^" : "*",
-	"--" : "-",
-	"++" : "+",
-	"%" : "/",
-	"<=" : "=",
-	"#" : "//",
-	"repeat" : "for"
-}
+  'int': 'var',
+  'float': 'var',
+  'string': 'var',
+  '^': '*',
+  '--': '-',
+  '++': '+',
+  '%': '/',
+  '<=': '=',
+  '#': '//',
+  'repeat': 'for'
+};
 
-let string = "a: int <= 10; b: string <= 'Start '; # comment example repeat (int i = 0; i < 10; i++) {a <= a ++ i ^ 2;} b <= b ++ 'End';";
+let string = `a: int <= 10;
+b: string <= 'Start '; # comment example
+repeat(int i = 0; i < 10; i++) {
+  a <= a ++ i ^ 2;
+}
+b <= b ++ 'End';`;
 
 function changeValue(str) {
-	let newString = str.split(' ');
+  let newString = str.split(' ');
 
-	for (let i = 0; i < newString.length; i++) {
-		for (let key in dictionary) {
-			if (newString[i] === key) {
-				newString[i] = dictionary[key];
-			}
-		}
-	}
-	let readyStr = newString.join(" ");
-	return readyStr;
+  for (let i = 0; i < newString.length; i++) {
+    for (let key in dictionary) {
+      if (newString[i] === key) {
+        newString[i] = dictionary[key];
+      }
+    }
+  }
+  let readyStr = newString.join(' ');
+
+  readyStr = readyStr.replace(/\w: var/g, (match) => {
+    let arrayOfVariableAndVar = match.split(' ');
+    let beginDeleteFrom = 0;
+    let endDelete = -1;
+    let firstArrayItem = 0;
+    let secondArrayItem = 1;
+    return [arrayOfVariableAndVar[secondArrayItem], arrayOfVariableAndVar[firstArrayItem]
+      .slice(beginDeleteFrom, endDelete)].join(' ');
+  });
+
+  return readyStr;
 }
 
 console.log(changeValue(string));
